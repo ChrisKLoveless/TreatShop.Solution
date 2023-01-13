1fcd8599ace99565596a5a9c6f238b30a405d971
Chris Loveless <chriskloveless@gmail.com>
2023-01-13 14:39:02 -0800

    add styling to splash page

--- TreatShop/Controllers/HomeController.cs
+++ TreatShop/Controllers/HomeController.cs
@@ -5,10 +5,20 @@ namespace TreatShop.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly TreatShopContext _db;
+    public HomeController(TreatShopContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Treat[] treats = _db.Treats.ToArray();
+      Flavor[] flavors = _db.Flavors.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("Treats", treats);
+      model.Add("Flavors", flavors);
+      return View(model);
     }
   }
 }
